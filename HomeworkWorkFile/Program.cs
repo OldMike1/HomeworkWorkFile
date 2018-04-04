cdf439923d163f78f1380bcc0d02b078775829ba
mike19952006 <mike19952006@gmail.com>
2018-04-04 18:35:59 +0300

    StreamReader

--- HomeworkWorkFile/Program.cs
+++ HomeworkWorkFile/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HomeworkWorkFile
 {
+    interface IWriteToFile
+    {
+        void Write();
+    }
+
+    class WorkFile : IWriteToFile
+    {
+        public string Path { get; set; }
+        public void Write()
+        {
+            Path = @"C:\Users\NewComp\Desktop\NewFile123.txt";
+
+            string text = "";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(Path, Encoding.Default))
+                {
+                    text = sr.ReadToEnd();
+                }
+            
+                using (StreamWriter sw = new StreamWriter(Path, true, Encoding.Default))
+                {
+                    sw.WriteLine(text);
+                }
+                string text1 = Console.ReadLine();
+                using (StreamWriter sw = new StreamWriter(Path, true, Encoding.Default))
+                {
+                    sw.WriteLine(text1);
+                }
+
+                using (StreamReader sr = new StreamReader(Path))
+                {
+                    Console.WriteLine(sr.ReadToEnd()); 
+                }
+            }
+            catch   (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+
+    
+
     class Program
     {
         static void Main(string[] args)
         {
+            WorkFile workFile = new WorkFile();
+            workFile.Write();
         }
     }
 }
